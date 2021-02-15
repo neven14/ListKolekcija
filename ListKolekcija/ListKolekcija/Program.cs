a6ae3e5c412284931e55e2a029051526b2a0f33e
Neven <58472164+neven14@users.noreply.github.com>
2021-02-15 08:53:50 +0100

    Add files via upload

---
+++ ListKolekcija/ListKolekcija/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListKolekcija
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> brojevi = new List<int>();
+
+            while (true)
+            {
+                Console.Write("Unesite cijeli broj:");
+                int broj = int.Parse(Console.ReadLine());
+
+                if (broj == 0)
+                {
+                    break;
+                }
+
+                brojevi.Add(broj);
+            }
+            brojevi.Sort();
+            brojevi.Reverse();
+
+
+            Console.WriteLine("Uneseni brojevi pad.redoslijedom:");
+            foreach (int broj in brojevi)
+            {
+                Console.Write(broj + ", ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
